efcf13820878fefd8946c01ba13e4b53a13f24c8
Florencia Bazan <florenciacbazan@gmail.com>
2022-08-30 13:04:25 -0300

    add New component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
+import NavBar from './components/NavBar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">SWEET</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" href="#">BIG SALE</a>
-              <a class="nav-link" href="#">WINTER 22</a>
-              <a class="nav-link" href="#">LOCALES</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-      </header>
-    </div>
+    <NavBar />
   );
 }
 
